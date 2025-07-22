fc298aaf7623eee5822e7a39682de31c4fa4b796
Márcio Teodoro <149595409+sanallite@users.noreply.github.com>
2025-07-22 18:57:10 -0300

    Jogo funcional

--- React/app01_JogoDaVelha/src/App.tsx
+++ React/app01_JogoDaVelha/src/App.tsx
@@ -2,23 +2,79 @@ import { useState } from 'react'
 
 import './App.css'
 
-function Quadrado({ valor, aoClicarQuad }: { valor: 'X' | 'O' | null, aoClicarQuad: () => void }) {
+type xisOuBolinha = 'X' | 'O' | null
+
+interface propsTabuleiro { 
+  xIsProximo: boolean,
+  quadrados: xisOuBolinha[],
+  onPlay: (arg: xisOuBolinha[]) => void 
+}
+
+function Quadrado({ valor, aoClicarQuad }: { valor: xisOuBolinha, aoClicarQuad: () => void }) {
   return (
     <button className='quadrado' onClick={ aoClicarQuad } >{valor}</button>
   )
 }
 
-function Tabuleiro() {
-  const [quadrados, setQuadrados] = useState(Array<'X' | 'O' | null>(9).fill(null))
+function calcularVencedor(quadrados: Array<xisOuBolinha>) {
+  const linhas = [
+    [0, 1, 2], // linhas horizontais
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], // linhas verticais
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8], // linhas diagonais
+    [2, 4, 6]
+  ]
 
+  for (let i = 0; i < linhas.length; i++) {
+    const [a, b, c] = linhas[i]
+
+    console.log(i, quadrados[a], quadrados[b], quadrados[c])
+
+    if (quadrados[a] && quadrados[a] === quadrados[b] && quadrados[a] === quadrados[c]) {
+      return quadrados[a]
+    }
+  }
+  
+  return null
+}
+
+function Tabuleiro({xIsProximo, quadrados, onPlay}: propsTabuleiro) {
   function handleClick(i: number): void {
+    if (quadrados[i] || calcularVencedor(quadrados)) {
+      return
+    }
+
     const proximosQuadrados = quadrados.slice()
-    proximosQuadrados[i] = 'X'
-    setQuadrados(proximosQuadrados)
+
+    if ( xIsProximo ) {
+      proximosQuadrados[i] = 'X'
+    }
+    
+    else {
+      proximosQuadrados[i] = 'O'
+    }
+
+    onPlay(proximosQuadrados)
+  }
+
+  const vencedor = calcularVencedor(quadrados)
+  let status: string
+
+  if (vencedor) {
+    status = 'O vencedor do jogo é '+vencedor
+  }
+
+  else {
+    status = 'O próximo jogador é '+(xIsProximo ? 'X' : 'O')
   }
 
   return (
     <>
+      <div className='status'>{status}</div>
+
       <div className='linha'>
         <Quadrado valor={ quadrados[0] } aoClicarQuad={ () => handleClick(0) }/>
         <Quadrado valor={ quadrados[1] } aoClicarQuad={ () => handleClick(1) }/>
@@ -40,4 +96,51 @@ function Tabuleiro() {
   )
 }
 
-export default Tabuleiro
+function Jogo() {
+  const [historico, setHistorico] = useState([Array<xisOuBolinha>(9).fill(null)])
+  const [turnoAtual, setTurno] = useState(0)
+  const quadradosAtuais = historico[turnoAtual]
+  const xIsProximo = turnoAtual % 2 === 0
+  
+  function handlePlay(proximosQuadrados: xisOuBolinha[]) {
+    const novoHistorico = [ ...historico.slice(0, turnoAtual + 1), proximosQuadrados ]
+    setHistorico(novoHistorico)
+    setTurno(novoHistorico.length - 1)
+  }
+
+  function pularPara(proximoTurno: number) {
+    setTurno(proximoTurno)
+  }
+
+  const movimentos = historico.map((quadrados, turno) => {
+    let descricao: string
+
+    if ( turno > 0 ) {
+      descricao = `Ir para o turno ${turno}`
+    }
+
+    else {
+      descricao = `Voltar para o início do jogo`
+    }
+
+    return (
+      <li key={ turno }>
+        <button onClick={ () => pularPara(turno) }>{ descricao }</button>
+      </li>
+    )
+  })
+
+  return (
+    <div className='jogo'>
+      <div className='tabuleiro'>
+        <Tabuleiro xIsProximo={ xIsProximo } quadrados={ quadradosAtuais } onPlay={ handlePlay } />
+      </div>
+
+      <div className='info-jogo'>
+        <ol>{ movimentos }</ol>
+      </div>
+    </div>
+  )
+}
+
+export default Jogo
